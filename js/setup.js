235076aec89adaf697d328f077c1d4c7b71ba9af
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2021-06-04 21:05:58 -0400

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -10,8 +10,8 @@ function setup() {
     const slAnimeTitleSelect = document.getElementById("slAnimeTitleSelect");
     const slPlayerName = document.getElementById("slPlayerName");
 
-    //const slInfo = document.getElementById("slInfo");
-    //const slScoreboard = document.getElementById("slScoreboard");  
+    const slInfo = document.getElementById("slInfo");
+    const slScoreboard = document.getElementById("slScoreboard");  
     
     const slTable = document.getElementById("slTable");
 
@@ -48,8 +48,8 @@ function setup() {
             let reader = new FileReader();
             reader.onload = function () {
                 try {
-                    //slInfo.style.display = 'none';
-                    //slScoreboard.style.display = 'none';
+                    slInfo.style.display = 'none';
+                    slScoreboard.style.display = 'none';
 
                     importData = JSON.parse(reader.result);
 
